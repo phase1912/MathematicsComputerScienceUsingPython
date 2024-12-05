d7f832973526f3fd6ea267c45c9b0eb23a5b9bef
Bohdan Razhyk <>
2024-12-05 09:52:05 +0100

    added examples

---
+++ examples/module8/imperic_gistogram.py
@@ -0,0 +1,232 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+#Під'єднуємось до гугл диску
+from google.colab import drive
+drive.mount('/content/drive')
+
+file_path = '/content/drive/MyDrive/ds/mat_stat.csv'
+
+
+# Створи об'єкт, який міститиме в собі наш датасет
+value_data = pd.read_csv(file_path, sep = ';')
+
+
+value_data.describe()
+# value_data.head()
+print(value_data.Q106)
+print(len(value_data.Q106))
+
+N=len(value_data.Q106) # розмір вибірки
+X = value_data.Q106
+
+
+# формується 2 масиви: n - кількість елементів, що попали в інтервал, x - масив меж інтервалів
+print(np.histogram(X, bins=13))
+n,x=np.histogram(X, bins=13)
+
+
+# масив початків інтервалів (прибрали останнє, найбільше значення)
+xmin=x[:-1]
+
+
+# ширина інтервалу (різниця двох послідовних стовпчиків).
+dx=x[1]-x[0]
+
+
+# емпірична приведена частота: частка від кількості елементів в інтервалі та загальної кількості елементів.
+y=n/N
+
+
+# масив центрів інтервалів
+xmid=xmin+dx/2
+# виводимо емпіричну гістограму приведених частот
+plt.bar(xmid, y, width=dx, color='y')
+
+
+# Виводимо підписи на осях
+plt.xlabel('x')
+plt.ylabel('Частота (ймовірність)')
+
+
+# Малюємо сітку та гістограму.
+plt.grid()
+plt.show()
+
+#############
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from scipy import stats
+
+
+N=len(value_data.Q106) # розмір вибірки
+X = value_data.Q106
+
+
+# формується 2 масиви: n - кількість елементів, що попали в інтервал, x - масив меж інтервалів
+print(np.histogram(X, bins=13))
+n,x=np.histogram(X, bins=13)
+
+
+mean,std = stats.norm.fit(X)
+
+
+# масив початків інтервалів (прибрали останнє, найбільше значення)
+xmin=x[:-1]
+
+
+# ширина інтервалу (різниця двох послідовних стовпчиків).
+dx=x[1]-x[0]
+
+
+# емпірична приведена частота: частка від кількості елементів в інтервалі та загальної кількості елементів.
+y=n/N
+
+
+# масив центрів інтервалів
+xmid=xmin+dx/2
+# виводимо емпіричну гістограму приведених частот
+plt.bar(xmid, y, width=dx, color='y')
+
+
+X1=np.linspace(-2,10,1000)
+dist2 = stats.norm(loc=mean, scale=std)
+plt.plot(X1, dist2.pdf(X1),'k-')
+
+
+
+# Виводимо підписи на осях
+plt.xlabel('x')
+plt.ylabel('Частота (ймовірність)')
+
+
+# Малюємо сітку та гістограму.
+plt.grid()
+plt.show()
+
+###
+
+.normaltest(X) # тест на нормальний розподіл (k2 - сума квадратів коефіцієнтів асиметрії і ексцесу)
+print (f"Перевірка на відповідність нормальному розподілу: {k2},pvalue = {pvalue}<0.05") # наприклад, якщо pvalue < 0.05, то це не нормальний розподіл
+
+
+# масив початків інтервалів (прибрали останнє, найбільше значення)
+xmin=x[:-1]
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from scipy import stats
+
+
+N=len(value_data.Q106) # розмір вибірки
+X = value_data.Q106
+
+
+# формується 2 масиви: n - кількість елементів, що попали в інтервал, x - масив меж інтервалів
+print(np.histogram(X, bins=13))
+n,x=np.histogram(X, bins=13)
+
+
+mean,std = stats.norm.fit(X)
+
+
+
+k2,pvalue = stats
+# ширина інтервалу (різниця двох послідовних стовпчиків).
+dx=x[1]-x[0]
+
+
+# емпірична приведена частота: частка від кількості елементів в інтервалі та загальної кількості елементів.
+y=n/N
+
+
+# масив центрів інтервалів
+xmid=xmin+dx/2
+# виводимо емпіричну гістограму приведених частот
+plt.bar(xmid, y, width=dx, color='y')
+
+
+X1=np.linspace(-2,10,1000)
+dist2 = stats.norm(loc=mean, scale=std)
+plt.plot(X1, dist2.pdf(X1),'k-')
+
+
+
+# Виводимо підписи на осях
+plt.xlabel('x')
+plt.ylabel('Частота (ймовірність)')
+
+
+# Малюємо сітку та гістограму.
+plt.grid()
+plt.show()
+
+##########
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from scipy import stats
+
+
+N=len(value_data.Q106) # розмір вибірки
+X_all = value_data.Q106
+
+
+# формується 2 масиви: n - кількість елементів, що попали в інтервал, x - масив меж інтервалів
+
+
+
+mean,std = stats.norm.fit(X)
+X= []
+for i in range(len(X_all)):
+  if 0<X_all[i]<3:
+    X.append(1);
+  if 3<=X_all[i]<=8:
+    X.append(2);
+  if 9<=X_all[i]<=10:
+    X.append(3);
+print(np.histogram(X, bins=3))
+n,x=np.histogram(X, bins=3)
+
+
+
+k2,pvalue = stats.normaltest(X) # тест на нормальний розподіл (k2 - сума квадратів коефіцієнтів асиметрії і ексцесу)
+print (f"Перевірка на відповідність нормальному розподілу: {k2},pvalue = {pvalue}>0.05") # наприклад, якщо pvalue > 0.05, то це не нормальний розподіл
+
+
+# масив початків інтервалів (прибрали останнє, найбільше значення)
+xmin=x[:-1]
+
+
+# ширина інтервалу (різниця двох послідовних стовпчиків).
+dx=x[1]-x[0]
+
+
+# емпірична приведена частота: частка від кількості елементів в інтервалі та загальної кількості елементів.
+y=n/N
+
+
+# масив центрів інтервалів
+xmid=xmin+dx/2
+# виводимо емпіричну гістограму приведених частот
+plt.bar(xmid, y, width=dx, color='y')
+
+
+X1=np.linspace(-2,10,1000)
+dist2 = stats.norm(loc=mean, scale=std)
+plt.plot(X1, dist2.pdf(X1),'k-')
+
+
+
+# Виводимо підписи на осях
+plt.xlabel('x')
+plt.ylabel('Частота (ймовірність)')
+
+
+# Малюємо сітку та гістограму.
+plt.grid()
+plt.show()
---
+++ examples/module8/statistic.py
@@ -0,0 +1,69 @@
+﻿n=2018
+d = 0.023
+dn=d*n
+print(dn)
+
+#####
+
+import math
+x = [113,114,106,108,120,104,116,112,110,118,103,120]
+y = [10,15,20,15,24,11,20,20,18,24,14,27]
+
+
+# Обраховуємо суму та середнє значення
+sum_x = sum(x)
+avg_x = sum(x)/len(x)
+sum_y = sum(y)
+avg_y = sum(y)/len(y)
+
+
+# Обраховуємо ряд, який є різницею між первинним рядом та середнім значенням ряду
+x_minus_avg = []
+y_minus_avg = []
+for i in range(len(x)):
+  x_minus_avg.append(x[i]-avg_x)
+  y_minus_avg.append(y[i]-avg_y)
+
+
+# Обраховуємо квадрати різниці значень ряду та її середніх, і добуток різниць значень ряду та їх середніх різних змінних
+xma2 = []
+yma2 = []
+xma_yma = []
+for i in range(len(x_minus_avg)):
+  xma2.append(x_minus_avg[i]**2)
+  yma2.append(y_minus_avg[i]**2)
+  xma_yma.append(x_minus_avg[i]*y_minus_avg[i])
+
+
+# Обраховуємо суми отриманих рядів
+sum_xma2 = sum(xma2)
+sum_yma2 = sum(yma2)
+sum_xma_yma = sum(xma_yma)
+
+
+# Обраховуємо критерій кореляції Пірсона
+r_x_y = sum_xma_yma/(math.sqrt(sum_xma2*sum_yma2))
+print(r_x_y )
+
+##########
+
+import numpy as np
+import math
+
+
+import math
+x = np.array([113,114,106,108,120,104,116,112,110,118,103,120])
+y = np.array([10,15,20,15,24,11,20,20,18,24,14,27])
+
+
+r_xy = sum( (x - avg_x)*(y - avg_y))/math.sqrt(sum((x - np.average(x))**2)*sum((y - np.average(y))**2))
+print(r_xy)
+
+#########
+
+import pandas as pd
+from scipy.stats import pearsonr
+
+
+corr, _ = pearsonr(x, y)
+print('Критерій кореляції Пірсона: %.3f' % corr)
